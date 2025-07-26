f8b56a543a189832f77d6a041dc18e1c443fa079
Bartłomiej <bartomiejskowron@icloud.com>
2025-07-26 17:15:27 +0200

    Add main Spring Boot application class, refactor package name

---
+++ src/main/java/pl/skowrxn/cookie/CookieConsentApplication.java
@@ -0,0 +1,13 @@
+package pl.skowrxn.cookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieConsentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CookieConsentApplication.class, args);
+    }
+
+}
