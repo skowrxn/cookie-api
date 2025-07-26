e84cdae5bd025cae720744d0188493cc3b6ad627
Bartłomiej <bartomiejskowron@icloud.com>
2025-07-26 17:10:15 +0200

    Initial commit

---
+++ src/test/java/pl/skowrxn/cookie/cookieapi/CookieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.skowrxn.cookie.cookieapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookieApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
