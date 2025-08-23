d7655e852ec3b1f739b9644dc8d85866879558d8
Bartłomiej <bartomiejskowron@icloud.com>
2025-08-23 11:55:08 +0200

    Add Admin Entity

---
+++ src/main/java/pl/skowrxn/cookie/admin/entity/Admin.java
@@ -0,0 +1,37 @@
+package pl.skowrxn.cookie.admin.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users",
+        uniqueConstraints = {
+                @UniqueConstraint(columnNames = "username")
+        })
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "password")
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user")
+    private List<Website> websites;
+
+}
