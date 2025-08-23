e93e3752f009e2521aae77b8f82828e3ab3a5885
Bartłomiej <bartomiejskowron@icloud.com>
2025-08-23 12:33:00 +0200

    feature: enable JPA auditing in main application

--- src/main/java/pl/skowrxn/cookie/CookieConsentApplication.java
+++ src/main/java/pl/skowrxn/cookie/CookieConsentApplication.java
@@ -2,8 +2,10 @@ package pl.skowrxn.cookie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CookieConsentApplication {
 
     public static void main(String[] args) {
