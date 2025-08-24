df12ef42865bda9c927f1f4764fd480eac28404e
Bartłomiej <bartomiejskowron@icloud.com>
2025-08-24 20:17:04 +0200

    feat: add method in ConsentTypeRepository to find ConsentType by key

--- src/main/java/pl/skowrxn/cookie/consent/repository/ConsentTypeRepository.java
+++ src/main/java/pl/skowrxn/cookie/consent/repository/ConsentTypeRepository.java
@@ -3,8 +3,11 @@ package pl.skowrxn.cookie.consent.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.skowrxn.cookie.consent.entity.ConsentType;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ConsentTypeRepository extends JpaRepository<ConsentType, UUID> {
+
+    Optional<ConsentType> findConsentTypeByKey(String key);
 
 }
