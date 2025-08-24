19669529338b74a3f8a760f10af83c0729939196
Bartłomiej <bartomiejskowron@icloud.com>
2025-08-24 20:01:11 +0200

    feat: add method in WebsiteRepository to find Website by key

--- src/main/java/pl/skowrxn/cookie/admin/repository/WebsiteRepository.java
+++ src/main/java/pl/skowrxn/cookie/admin/repository/WebsiteRepository.java
@@ -3,7 +3,11 @@ package pl.skowrxn.cookie.admin.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.skowrxn.cookie.admin.entity.Website;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface WebsiteRepository extends JpaRepository<Website, UUID> {
+
+    Optional<Website> findWebsiteByKey(String key);
+
 }
