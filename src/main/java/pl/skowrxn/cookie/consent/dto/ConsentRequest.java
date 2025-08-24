91bc9bcc380d99af1de204b04074d56c11908862
Bartłomiej <bartomiejskowron@icloud.com>
2025-08-24 20:13:47 +0200

    feat: implement ConsentRequest for request validation

---
+++ src/main/java/pl/skowrxn/cookie/consent/dto/ConsentRequest.java
@@ -0,0 +1,29 @@
+package pl.skowrxn.cookie.consent.dto;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+public class ConsentRequest {
+
+    @NotNull(message = "Log cannot be null")
+    @NotEmpty(message = "Log cannot be empty")
+    @Valid
+    private List<ConsentLogDTO> log;
+
+    @NotBlank(message = "Website key is required")
+    private String key;
+
+    @NotBlank(message = "Consent ID is required")
+    private String consent_id;
+
+    @NotBlank(message = "Domain is required")
+    private String domain;
+}
