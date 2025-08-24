41c66a70e3e4a69c1dddf4d12dba1c5300cb73ed
Bartłomiej <bartomiejskowron@icloud.com>
2025-08-24 19:53:37 +0200

    feat: add public consent API with IP and user-agent logging

---
+++ src/main/java/pl/skowrxn/cookie/consent/controller/ConsentController.java
@@ -0,0 +1,28 @@
+package pl.skowrxn.cookie.consent.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.skowrxn.cookie.consent.dto.ConsentRequest;
+import pl.skowrxn.cookie.consent.service.ConsentService;
+
+@RestController
+@RequestMapping("api/v1/consents")
+@AllArgsConstructor
+public class ConsentController {
+
+    private ConsentService consentService;
+
+    @PostMapping
+    public ResponseEntity<?> saveConsent(
+            @RequestBody ConsentRequest consentRequest,
+            HttpServletRequest servletRequest
+    ) {
+        String ip = servletRequest.getRemoteAddr();
+        String userAgent = servletRequest.getHeader("User-Agent");
+        consentService.saveConsent(consentRequest, ip, userAgent);
+        return ResponseEntity.ok().build();
+    }
+
+}
