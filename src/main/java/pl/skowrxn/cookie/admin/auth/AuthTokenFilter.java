e85a8d3d2f41d975eccbcde819fdd4f99cb22d69
Bartłomiej <bartomiejskowron@icloud.com>
2025-09-03 15:37:03 +0200

    feat(auth): add AuthTokenFilter to handle JWT-based request authentication

---
+++ src/main/java/pl/skowrxn/cookie/admin/auth/AuthTokenFilter.java
@@ -0,0 +1,61 @@
+package pl.skowrxn.cookie.admin.auth;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    private final JWTUtils jwtUtils;
+    private final UserDetailsService userDetailsService;
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+
+    public AuthTokenFilter(JWTUtils jwtUtils, UserDetailsService userDetailsService) {
+        this.jwtUtils = jwtUtils;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String path = request.getRequestURI();
+        logger.debug("AuthTokenFilter called for URI: {}", request.getRequestURI());
+
+        if (path.startsWith("/api/v1/public")
+                || path.startsWith("/api/v1/auth/login")
+                || path.startsWith("/api/v1/auth/register")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = this.jwtUtils.getJWTFromCookies(request);
+
+        if (token != null && this.jwtUtils.validateJwtToken(token)) {
+            String email = this.jwtUtils.getEmailFromJWT(token);
+
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(email);
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    userDetails, null, userDetails.getAuthorities());
+
+            logger.debug("Roles from JWT for user {}: {}", email, userDetails.getAuthorities());
+
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+}
