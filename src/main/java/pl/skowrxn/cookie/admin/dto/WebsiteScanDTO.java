64563547918ec6454580436e5078ec91379e9465
Bartłomiej <bartomiejskowron@icloud.com>
2025-09-07 17:20:42 +0200

    feat: update WebsiteScanDTO to use CookieTypeDTO for detectedCookieTypes

--- src/main/java/pl/skowrxn/cookie/admin/dto/WebsiteScanDTO.java
+++ src/main/java/pl/skowrxn/cookie/admin/dto/WebsiteScanDTO.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import pl.skowrxn.cookie.consent.entity.CookieType;
+import pl.skowrxn.cookie.consent.dto.CookieTypeDTO;
 
 import java.time.Instant;
 import java.util.List;
@@ -17,7 +17,7 @@ import java.util.UUID;
 public class WebsiteScanDTO {
 
     private UUID id;
-    private List<CookieType> detectedCookieTypes;
+    private List<CookieTypeDTO> detectedCookieTypes;
     private Instant scanDate;
     private boolean isSuccessful;
     private int totalCookies;
