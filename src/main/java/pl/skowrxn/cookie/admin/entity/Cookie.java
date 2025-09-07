ae452ffecd9c965c6c942661fac5960f20a3e386
Bartłomiej <bartomiejskowron@icloud.com>
2025-09-07 19:21:11 +0200

    feat: add many-to-many relationship with CookieType, add new constructor add column names

--- src/main/java/pl/skowrxn/cookie/admin/entity/Cookie.java
+++ src/main/java/pl/skowrxn/cookie/admin/entity/Cookie.java
@@ -1,31 +1,48 @@
 package pl.skowrxn.cookie.admin.entity;
 
 import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+import org.jsoup.nodes.Element;
 import pl.skowrxn.cookie.consent.entity.CookieType;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
+@Table(name = "cookies")
+@NoArgsConstructor
 public class Cookie {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @Column(nullable = false)
+    @Column(name = "name", nullable = false)
     private String name;
 
-    @Column
+    @Column(name = "domain")
     private String domain;
 
-    @Column
+    @Column(name = "description", length = 1024)
     private String description;
 
-    @Column
+    @Column(name = "duration")
     private String duration;
 
-    @ManyToOne
-    @JoinColumn(name = "cookie_type_id")
-    private CookieType cookieType;
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(
+            name = "cookie_cookie_types",
+            joinColumns = @JoinColumn(name = "cookie_id"),
+            inverseJoinColumns = @JoinColumn(name = "cookie_type_id")
+    )
+    private List<CookieType> cookieTypes;
 
+    public Cookie(String name, String domain, String description, String duration) {
+        this.name = name;
+        this.domain = domain;
+        this.description = description;
+        this.duration = duration;
+        this.cookieTypes = new ArrayList<>();
+    }
 }
