95a796eb1e4d76a3e3c6400d279efda5e660a8d8
Bartłomiej <bartomiejskowron@icloud.com>
2025-09-07 19:22:10 +0200

    feat: simplify CookieDTO by using @Data annotation and adding a new constructor

--- src/main/java/pl/skowrxn/cookie/admin/dto/CookieDTO.java
+++ src/main/java/pl/skowrxn/cookie/admin/dto/CookieDTO.java
@@ -1,14 +1,12 @@
 package pl.skowrxn.cookie.admin.dto;
 
-import lombok.NoArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.UUID;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class CookieDTO {
 
     private UUID id;
@@ -17,5 +15,12 @@ public class CookieDTO {
     private String description;
     private String duration;
     private String type;
-    
+
+    public CookieDTO(String cookieKey, String domain, String expiration, String description, String type) {
+        this.name = cookieKey;
+        this.domain = domain;
+        this.duration = expiration;
+        this.description = description;
+        this.type = type;
+    }
 }
