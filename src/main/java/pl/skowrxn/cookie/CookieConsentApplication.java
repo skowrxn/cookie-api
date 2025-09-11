146eac926838111832594ca48af906e7c6e5a317
Bartłomiej <bartomiejskowron@icloud.com>
2025-09-11 02:30:46 +0200

    feat: add modelmapper configuration

--- src/main/java/pl/skowrxn/cookie/CookieConsentApplication.java
+++ src/main/java/pl/skowrxn/cookie/CookieConsentApplication.java
@@ -1,5 +1,7 @@
 package pl.skowrxn.cookie;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -12,6 +14,16 @@ public class CookieConsentApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CookieConsentApplication.class, args);
+    }
+
+    public static final ModelMapper modelMapper;
+
+    static {
+        modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE);
     }
 
 }
