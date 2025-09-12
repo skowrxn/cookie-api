6f7b401a42bf447ca913cbd005413c00ff4d9785
Bartłomiej <bartomiejskowron@icloud.com>
2025-09-12 22:34:28 +0200

    feat: add BannerSettingsDTO

---
+++ src/main/java/pl/skowrxn/cookie/admin/dto/BannerSettingsDTO.java
@@ -0,0 +1,23 @@
+package pl.skowrxn.cookie.admin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BannerSettingsDTO {
+
+    private String rejectAllButtonText;
+    private String acceptAllButtonText;
+    private String saveButtonText;
+    private String manageCookiesButtonText;
+    private String bannerDescriptionText;
+    private String bannerTitleText;
+    private String primaryColor;
+    private String textColor;
+
+}
