ceb7a58aeb00cf1cf1a2a73b7daf8328a14ee34b
Bartłomiej <bartomiejskowron@icloud.com>
2025-09-12 22:29:50 +0200

    feat: add methods to CookieTypeRepository for querying by Website

--- src/main/java/pl/skowrxn/cookie/common/repository/CookieTypeRepository.java
+++ src/main/java/pl/skowrxn/cookie/common/repository/CookieTypeRepository.java
@@ -1,8 +1,10 @@
 package pl.skowrxn.cookie.common.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pl.skowrxn.cookie.admin.entity.Website;
 import pl.skowrxn.cookie.common.entity.CookieType;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -11,5 +13,9 @@ public interface CookieTypeRepository extends JpaRepository<CookieType, UUID> {
     Optional<CookieType> findCookieTypeById(UUID id);
 
     Optional<CookieType> findCookieTypeByKey(String key);
+
+    Optional<CookieType> findCookieTypeByWebsiteAndKey(Website website, String key);
+
+    List<CookieType> findCookieTypesByWebsite(Website website);
 
 }
