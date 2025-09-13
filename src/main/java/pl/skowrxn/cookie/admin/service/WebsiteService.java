9007cc1e70f0df16a16cdbd02aa7dd14b80c21a6
Bartłomiej <bartomiejskowron@icloud.com>
2025-09-13 15:24:15 +0200

    feat: add methods to get list of user's websites and get Website Entity by id, rename delete method

--- src/main/java/pl/skowrxn/cookie/admin/service/WebsiteService.java
+++ src/main/java/pl/skowrxn/cookie/admin/service/WebsiteService.java
@@ -2,13 +2,15 @@ package pl.skowrxn.cookie.admin.service;
 
 import pl.skowrxn.cookie.admin.dto.WebsiteDTO;
 import pl.skowrxn.cookie.admin.entity.User;
+import pl.skowrxn.cookie.admin.entity.Website;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 public interface WebsiteService {
 
-    void deleteWebsiteDataById(UUID id);
+    void deleteWebsiteById(UUID id);
 
     WebsiteDTO getWebsiteById(UUID id);
 
@@ -19,4 +21,9 @@ public interface WebsiteService {
     WebsiteDTO setLastSuccessfulScanTime(UUID id, Instant time);
 
     WebsiteDTO setBannerStatus(UUID id, boolean isActive);
+
+    List<WebsiteDTO> getWebsitesByUserId(UUID id);
+
+    Website getWebsiteEntityById(UUID id);
+
 }
