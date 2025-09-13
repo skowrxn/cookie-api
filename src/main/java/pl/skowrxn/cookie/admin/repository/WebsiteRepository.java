39ff07163361c554c26b2c69376a540c372f495d
Bartłomiej <bartomiejskowron@icloud.com>
2025-09-13 15:26:19 +0200

    feat: add method to retrieve websites by user ID in WebsiteRepository

--- src/main/java/pl/skowrxn/cookie/admin/repository/WebsiteRepository.java
+++ src/main/java/pl/skowrxn/cookie/admin/repository/WebsiteRepository.java
@@ -3,11 +3,14 @@ package pl.skowrxn.cookie.admin.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.skowrxn.cookie.admin.entity.Website;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface WebsiteRepository extends JpaRepository<Website, UUID> {
 
     Optional<Website> findWebsiteByKey(String key);
+
+    List<Website> getWebsitesByUser_Id(UUID userId);
 
 }
