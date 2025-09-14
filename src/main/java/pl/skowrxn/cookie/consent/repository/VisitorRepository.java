0a84ab69581accae3558fbce1edc1a906726cf49
Bartłomiej <bartomiejskowron@icloud.com>
2025-09-14 22:18:46 +0200

    refactor: add methods to retrieve single/multiple visitors, include websiteId in the query

--- src/main/java/pl/skowrxn/cookie/consent/repository/VisitorRepository.java
+++ src/main/java/pl/skowrxn/cookie/consent/repository/VisitorRepository.java
@@ -1,5 +1,7 @@
 package pl.skowrxn.cookie.consent.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.skowrxn.cookie.consent.entity.Visitor;
 
@@ -8,6 +10,10 @@ import java.util.UUID;
 
 public interface VisitorRepository extends JpaRepository<Visitor, UUID> {
 
-    Optional<Visitor> findVisitorByToken(String token);
-    
+    Optional<Visitor> findVisitorByWebsite_IdAndConsentId(UUID websiteId, String consentId);
+
+    Optional<Visitor> findVisitorByIdAndWebsite_Id(UUID visitorId, UUID websiteId);
+
+    Page<Visitor> findVisitorByWebsite_Id(UUID websiteId, Pageable pageable);
+
 }
